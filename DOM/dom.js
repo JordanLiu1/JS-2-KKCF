19d3138bf5d81f0868840c588d20b9f8c17f4862
JordanLiu1 <1680jordan@gmail.com>
2024-08-05 17:10:38 +0000

    changes

--- DOM/dom.js
+++ DOM/dom.js
@@ -3,18 +3,22 @@
 // 1. select the element with the id 'title' using getElementById
 
 // 2.  save the reference to the element with the id 'title' to a variable
-
+let myTitle = document.getElementById("title")
 // 3. console log the new variable 
-
+console.log(myTitle)
 // 4. Use query selector to select the first h2 
-document.querySelector()
+document.querySelector("h2")
 
 //5. Use query selector to select the paragraph by its class of 'description'
-
+document.querySelector("description")
 // 6. Use query selector all to select all the h2's on the page
+document.querySelectorAll("h2")
 
+document.querySelector("#transform").innerText = 'Sailor Moon'
 //7. Exercise 1 Create at least 3 different elements in the body of your html file
+
 // In your JS file select each element and log them to the console 
+
 // Select multiple elements with querySelectorAll() and log them to the console
 
 // 8. use innerText property to change the text of the h1 with the id 'transform
